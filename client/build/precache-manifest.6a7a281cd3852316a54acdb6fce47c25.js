02300d3f625422dd7911c9b6c7f5121081cb8e28
abhishekkranjan <abhishek.kvj.2@gmail.com>
2022-05-24 01:26:55 +0530

    Client Handled

---
+++ client/build/precache-manifest.6a7a281cd3852316a54acdb6fce47c25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eef9a5694e6457af6c34aea9440c9639",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6cb5d8df7aad75a28431",
+    "url": "/static/css/main.9a37f095.chunk.css"
+  },
+  {
+    "revision": "53b14e5bf8147e28cff6",
+    "url": "/static/js/2.dfe48f4d.chunk.js"
+  },
+  {
+    "revision": "6a3fd5f48a97c7577e9c3ede94b4a5b5",
+    "url": "/static/js/2.dfe48f4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cb5d8df7aad75a28431",
+    "url": "/static/js/main.5995f286.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/memories.5c0c63fb.png"
+  }
+]);
